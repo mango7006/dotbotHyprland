ff3c571bfc7ebc73a828136f3649319f72a99c62
mango7006 <magnus.van.der.wal@outlook.com>
2025-06-16 00:03:42 +0200

    dotbot

--- config/scripts/weather-buienradar.sh
+++ config/scripts/weather-buienradar.sh
@@ -2,6 +2,6 @@
 
 TEMP=$(curl -s https://gadgets.buienradar.nl/gadget/weathersymbol | grep -oP 'title="Huidige temperatuur: \K[0-9]+(\.[0-9]+)?' | awk '{print $0 "°C"}')
 
-ICON=$(curl -s "wttr.in/?format=%c")
+ICON=$(curl -s "wttr.in/?format=%c" | sed 's/+//g; s/C/C /g; s/ //g')
 
 echo $TEMP$ICON
