94e90c3f2ed157bb90f53103fe0a584a8b07468c
mango7006 <magnus.van.der.wal@outlook.com>
2025-06-20 12:07:55 +0200

    dotbot

--- config/scripts/weather-buienradar.sh
+++ config/scripts/weather-buienradar.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-TEMP=$(curl -s https://gadgets.buienradar.nl/gadget/weathersymbol | grep -oP 'title="Huidige temperatuur: \K[0-9]+(\.[0-9]+)?' | awk '{print $0 "°C"}' || echo "Could not resolve")
+round() {
+  local input="$1"
+  printf "%.0f\n" "$input"
+}
+
+TEMP=$(curl -s https://gadgets.buienradar.nl/gadget/weathersymbol | grep -oP 'title="Huidige temperatuur: \K[0-9]+(\.[0-9]+)?')
 
 ICON=$(curl -s "wttr.in/?format=%c" | sed 's/+//g; s/C/C /g; s/ //g')
 
-echo $TEMP$ICON
+ROUNDED=$(round $TEMP | awk '{print $0 "°C"}')
+
+echo $ROUNDED$ICON
