a3ae3e5970e398e73b06dfc5a2f8c049f2b96409
mango7006 <magnus.van.der.wal@outlook.com>
2025-06-22 14:32:50 +0200

    dotbot

--- config/scripts/keyboard-hyprland.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This keyboard switcher is Hyprland only, disable or change module with other WM's
-
-if hyprctl devices | grep -q "intl., with"; then
-  echo "intl"
-else
-  echo "us"
-fi
