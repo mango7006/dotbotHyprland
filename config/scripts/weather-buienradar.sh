823976153678d0e8a96f1fcb8d001758e5498fbe
mango7006 <magnus.van.der.wal@outlook.com>
2025-07-01 21:32:57 +0200

    Revert "..."
    This reverts commit b1250f766c99e93ef7c40afcec6960ba92216771.
    
    modified:   config/nvim/lazy-lock.json
    new file:   config/scripts/weather-buienradar.sh
    modified:   config/waybar/config.jsonc
    modified:   config/waybar/style.css
    modified:   home/.zshrc

---
+++ config/scripts/weather-buienradar.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This script is NL/buienradar only, change the waybar module to be custom/wttrINT for international weather
+
+TEMP=$(curl -s https://gadgets.buienradar.nl/gadget/weathersymbol | grep -oP 'title="Huidige temperatuur: \K[0-9]+(\.[0-9]+)?')
+
+ICON=$(curl -s "wttr.in/?format=%c" | sed 's/+//g; s/C/C /g; s/ //g')
+
+ROUNDED=$(printf "%.0f°C" "$TEMP")
+
+echo ""$ROUNDED""$ICON""
--- home/.zshrc
+++ home/.zshrc
@@ -40,7 +40,7 @@ cd() {
 }
 
 AI() {
-  source ~/Code/RobotAi/venv/bin/activate; python3 ~/Code/RobotAi/AAI.py 2>/dev/null; deactivate
+  source ~/Python/RobotAi/venv/bin/activate; python3 ~/Python/RobotAi/AAI.py 2>/dev/null; deactivate
 }
 
 alias cp="cp -v"
