392c89d9bedb98c9126c3ed938f08ba8071f6b6d
mango7006 <magnus.van.der.wal@outlook.com>
2025-07-28 17:32:06 +0200

    small things

--- config/scripts/actions-menu.sh
+++ config/scripts/actions-menu.sh
@@ -6,24 +6,24 @@ options=$(
 Wake PC and Login
 Toggle Light Filter
 Toggle Recording
-Toggle Bluetooth
 Toggle Focus Mode
 Toggle Wireguard
 Reload Waybar
 Update Everything
 Clean Temporary Files
-Clean Clipboard History
 EOF
 )
 
 ## Extra options
 # Toggle Playerctl
+# Toggle Bluetooth
+# Clean Clipboard History
 # Toggle Keyboard Layout
 # Toggle Network
 
 # Show wofi menu
 # 41 pixels per option entry, i.e. 41 * 7 = 287
-choice=$(echo -e "$options" | wofi -H 410 --sort-order=default --cache-file=/dev/null --dmenu --prompt "Select Action")
+choice=$(echo -e "$options" | wofi -H 323 --sort-order=default --cache-file=/dev/null --dmenu --prompt "Select Action")
 
 # Run the corresponding command
 # These should not be commented, only in the options variable
