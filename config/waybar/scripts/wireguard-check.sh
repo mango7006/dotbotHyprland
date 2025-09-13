f138faf339c500a8ff06b44f57387e3eb1a71c52
mango7006 <magnus.van.der.wal@outlook.com>
2025-09-13 11:40:54 +0200

    opdracht 2 af denk ik

--- config/waybar/scripts/wireguard-check.sh
+++ config/waybar/scripts/wireguard-check.sh
@@ -3,11 +3,9 @@
 # Check if WireGuard is active and not empty
 if [ -n "$(wg show interfaces)" ]; then
   STATUS="$(wg show interfaces)"
-  PUBLIC_IP=$(curl -4 -s --max-time 2 https://api.ipify.org || echo "timeout")
   # Json format
-  echo "{\"text\":\"$STATUS\",\"tooltip\":\"IP: $PUBLIC_IP\",\"alt\":\"$PUBLIC_IP\"}"
+  echo "{\"text\":\"$STATUS\"}"
 else
-  PUBLIC_IP=$(curl -4 -s --max-time 2 ifconfig.me)
   # Json format
-  echo "{\"text\":\"inactive\",\"tooltip\":\"IP: $PUBLIC_IP\",\"alt\":\"$PUBLIC_IP\"}"
+  echo "{\"text\":\"inactive\"}"
 fi
