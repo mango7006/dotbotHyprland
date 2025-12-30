ef512fb3389d9b7565da929242ef3262ff7a013b
mango7006 <magnus.van.der.wal@outlook.com>
2025-12-30 17:32:54 +0100

    comments toegevoegd

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,17 +3,6 @@ require("config.lazy")
 require("highlight-undo").setup({})
 require("urlview").setup({})
 vim.o.scrolloff = 15
-vim.filetype.add({
-    extension = { rasi = "rasi" },
-    pattern = {
-        [".*/waybar/config"] = "jsonc",
-        [".*/dunst/dunstrc"] = "hyprlang",
-        [".*/kitty/*.conf"] = "bash",
-        [".*/hypr/.*%.conf"] = "hyprlang",
-        [".*/mango/.*.conf"] = "hyprlang",
-        [".*/wofi/config"] = "hyprlang",
-    },
-})
 
 vim.opt.sidescrolloff = 8
 vim.opt.hlsearch = true
