5fdc18f74724a38c785d121fa75ff932331cfca5
mango7006 <magnus.van.der.wal@outlook.com>
2026-05-24 14:40:47 +0200

    last update

--- config/scripts/actions-menu.sh
+++ config/scripts/actions-menu.sh
@@ -2,11 +2,9 @@
 
 # Define menu options
 options=$(
-  cat <<EOF
+    cat <<EOF
 Toggle Light Filter
 VPN Menu
-NOS Teletekst
-Notes
 Reload Waybar
 Update Everything
 Clean Temporary Files
@@ -15,6 +13,8 @@ EOF
 )
 
 ## Extra options
+# Notes
+# NOS Teletekst
 # Wake PC and Login
 # Toggle Bluetooth
 # Clean Clipboard History
@@ -33,63 +33,63 @@ choice=$(echo -e "$options" | wofi -H $pixels --sort-order=default --cache-file=
 # These should not be commented, only in the options variable
 case "$choice" in
 "Toggle Bluetooth")
-  bluetooth toggle
-  ;;
+    bluetooth toggle
+    ;;
 "Toggle Wireguard")
-  kitty --class terminalfloatsmall -e ~/.config/scripts/wireguard-master.sh toggle RPI-Home
-  ;;
+    kitty --class terminalfloatsmall -e ~/.config/scripts/wireguard-master.sh toggle RPI-Home
+    ;;
 "Toggle Light Filter")
-  if pgrep gammastep >/dev/null; then
-    pkill gammastep
-  else
-    gammastep -O 4500 &
-  fi
-  ;;
+    if pgrep gammastep >/dev/null; then
+        pkill gammastep
+    else
+        gammastep -O 4500 &
+    fi
+    ;;
 "Switch Keyboard Layout")
-  hyprctl switchxkblayout at-translated-set-2-keyboard next
-  ;;
+    hyprctl switchxkblayout at-translated-set-2-keyboard next
+    ;;
 "Toggle Network")
-  rfkill toggle wlan
-  ;;
+    rfkill toggle wlan
+    ;;
 "Clean Temporary Files")
-  kitty --class terminalfloatsmall -e ~/.config/scripts/cleantmp.sh
-  ;;
+    kitty --class terminalfloatsmall -e ~/.config/scripts/cleantmp.sh
+    ;;
 "Wake PC and Login")
-  ~/.config/scripts/wake-pc.sh
-  ;;
+    ~/.config/scripts/wake-pc.sh
+    ;;
 "Update Everything")
-  kitty --class terminalfloat -e topgrade
-  ;;
+    kitty --class terminalfloat -e topgrade
+    ;;
 "Clean Clipboard History")
-  clipse -clear
-  ;;
+    clipse -clear
+    ;;
 "Reload Waybar")
-  pkill waybar
-  waybar &
-  ;;
+    pkill waybar
+    waybar &
+    ;;
 "Calculator")
-  kitty --class terminalfloat -e calc
-  ;;
+    kitty --class terminalfloat -e calc
+    ;;
 "NOS Teletekst")
-  kitty --class teletekst -e ssh teletekst.nl
-  ;;
+    kitty --class teletekst -e ssh teletekst.nl
+    ;;
 "VPN Menu")
-  ~/.config/scripts/wireguard-vpn-menu.sh
-  ;;
+    ~/.config/scripts/wireguard-vpn-menu.sh
+    ;;
 "Programming")
-  kitty /c/s4731689/
-  ;;
+    kitty /c/s4731689/
+    ;;
 "Copy Song Name")
-  song=$(playerctl metadata --format '{{title}}')
-  wl-copy $song
-  firefox --new-tab "https://translate.google.com/?sl=ru&tl=en&text=$song"
-  ;;
+    song=$(playerctl metadata --format '{{title}}')
+    wl-copy $song
+    firefox --new-tab "https://translate.google.com/?sl=ru&tl=en&text=$song"
+    ;;
 "Notes")
-  kitty ~/Sync/Obsidian/notes/02\ -\ School/
-  ;;
+    kitty ~/Sync/Obsidian/notes/02\ -\ School/
+    ;;
 *)
-  exit 1
-  ;;
+    exit 1
+    ;;
 esac
 
 ## Packages directly used in this script:
